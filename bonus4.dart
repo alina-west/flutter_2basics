a33d2b4aa2e9a257d1c47b2c585c9bf5fc47027f
alina-west <144222854+alina-west@users.noreply.github.com>
2024-04-24 13:12:50 +0500

    Update bonus4.dart

--- bonus4.dart
+++ bonus4.dart
@@ -1,1 +1,15 @@
+import 'dart:io';
 
+void main() {
+  print("градус манин жаз: ");
+  double ymani = double.parse(stdin.readLineSync()!);
+
+  int sagat = (ymani / 30).toInt();
+
+  int minuta = ((ymani % 30) * 2).toInt();
+
+  print(
+      "Since the beginning of the day $sagat hours and $minuta minutes were done");
+}
+//бизде 12 часовые делений бар жане 360 кун бар\ 360 / 12 = 30/ сол ушин 30 градус 1 сагат
+//toInt ол санга айналдырады\ типо intagerge
